10fd066c70df74c2701a29e518e913dc6a1d9a2b
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2024-09-25 14:32:50 +0100

    errmsg

--- app.js
+++ app.js
@@ -8,9 +8,14 @@
 
         async function checkWeather(city) {
             const response = await fetch(apiUrl + city + `&appid=${apikey}`);
-            var data = await response.json();
+            
+            if(response.status == 404){
+                document.querySelector(".error").style.display = "block";
+                document.querySelector(".weather").style.display = "none";
+            }
+            else{
+                var data = await response.json();
 
-            console.log(data);
 
             document.querySelector(".city").innerHTML = data.name;
             document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -19,7 +24,11 @@
         
             let weatherCondition = data.weather[0].main.toLowerCase();
                 weatherIcon.src = `assets/images/${weatherCondition}.png`;
-        }
+        
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
+            }
+            }
 
         searchBtn.addEventListener("click", ()=>{
             checkWeather(searchBox.value);
